8c942c88cfd9504a4158e798f7e1de38ee749a7c
Erik Herazo <erickherazoj@gmail.com>
2023-07-06 12:48:07 -0500

    commit 2

--- store/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from utils.mongo_utils import db
-from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_http_methods
-
-
-@csrf_exempt
-def index(request):
-    return(request)
