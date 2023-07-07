c779079f255935c24befb4373209aec2805eea72
Erik Herazo <erickherazoj@gmail.com>
2023-07-07 14:44:58 -0500

    commit 3

--- APIS/user/admin.py
+++ APIS/user/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ModelUser
+
+@admin.register(ModelUser)
+class ModelUserAdmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'last_name', 'username', 'email'
+
+    class Meta:
+        db_table = 'store_users'
+        managed = True
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
